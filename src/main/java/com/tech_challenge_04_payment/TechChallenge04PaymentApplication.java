56ad7d252f4fd7a482f2236c7dc73ad18a117f41
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-06-16 11:03:16 -0300

    first commit

---
+++ src/main/java/com/tech_challenge_04_payment/TechChallenge04PaymentApplication.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge04PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge04PaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech_challenge_04_payment/TechChallenge04PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech_challenge_04_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge04PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
